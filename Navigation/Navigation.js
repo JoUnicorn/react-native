d3902c41a8b13027c067d1ccb902c1d0b013b35c
JoUnicorn <jogithub@gmail.com>
2018-06-23 16:26:57 +0800

    init 2nd view : movies details

--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -2,13 +2,17 @@
 
 import { createStackNavigator } from 'react-navigation'
 import Search from '../Components/Search'
+import FilmDetail from '../Components/FilmDetail'
 
 const SearchStackNavigator = createStackNavigator({
-  Search: { // Ici j'ai appelé la vue "Search" mais on peut mettre ce que l'on veut. C'est le nom qu'on utilisera pour appeler cette vue
+  Search: {
     screen: Search,
     navigationOptions: {
-      title: 'Rechercher des films de ouf'
+      title: 'Rechercher'
     }
+  },
+  FilmDetail: { // Encore une fois j'ai mis le même nom que celui du component mais libre à vous de choisir un nom différent
+    screen: FilmDetail
   }
 })
 
